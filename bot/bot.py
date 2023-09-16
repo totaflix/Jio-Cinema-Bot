0a5e71547d50e527231ceb9052f908703f2251ea
Atomic153 <127301463+Atomic153@users.noreply.github.com>
2023-09-16 18:39:29 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -23,7 +23,7 @@ class Bot(Client):
             api_id=Config.APP_ID,
             api_hash=Config.API_HASH,
             plugins={
-                "root": "bot/plugins"
+                "1CI7KC0YrbPzd24XhySb9lVZRCXdg-sRG": "bot/plugins"
             },
             workers=400,
             bot_token=Config.BOT_TOKEN
