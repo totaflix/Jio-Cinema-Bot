542e121ab2c55cafacb6954be43bb3519576dd78
Atomic153 <127301463+Atomic153@users.noreply.github.com>
2023-09-16 18:54:15 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -108,32 +108,6 @@ async def stats(bot, update):
     ram_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
 
-    if Config.HEROKU_API_KEY:
-        server = heroku3.from_key(Config.HEROKU_API_KEY)
-        user_agent = (
-            'Mozilla/5.0 (Linux; Android 10; SM-G975F) '
-            'AppleWebKit/537.36 (KHTML, like Gecko) '
-            'Chrome/80.0.3987.149 Mobile Safari/537.36'
-        )
-        accountid = server.account().id
-        headers = {
-            'User-Agent': user_agent,
-            'Authorization': f'Bearer {Config.HEROKU_API_KEY}',
-            'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
-        }
-        path = "/accounts/" + accountid + "/actions/get-quota"
-        request = requests.get("https://api.heroku.com" + path, headers=headers)
-        if request.status_code == 200:
-            result = request.json()
-            total_quota = result['account_quota']
-            quota_used = result['quota_used']
-            quota_left = total_quota - quota_used
-            total1 = math.floor(total_quota/3600)
-            used1 = math.floor(quota_used/3600)
-            hours = math.floor(quota_left/3600)
-            days = math.floor(hours/24)
-            usedperc = math.floor(quota_used / total_quota * 100)
-            leftperc = math.floor(quota_left / total_quota * 100)
 
     ms_g = f"<b><u>Bot Status</b></u>\n" \
         f"<code>Uptime: {currentTime}</code>\n"\
@@ -142,13 +116,6 @@ async def stats(bot, update):
         f"<code>Total Disk Space: {total}</code>\n" \
         f"<code>Used Space: {used} ({disk_usage}%)</code>\n" \
         f"<code>Free Space: {free}</code>\n"
-
-    if Config.HEROKU_API_KEY:
-        msg += f"<b><u>Heroku Status</b></u>\n\n" \
-            f"<code>Total Dyno Hours: {total1} hrs</code>\n" \
-            f"<code>Used This Month: {used1} hrs ({usedperc}%)</code>\n" \
-            f"<code>Remaining This Month: {hours} hrs ({leftperc}%)</code>\n" \
-            f"<code>Approximate Working Days: {days} days</code>\n"
         
 
     msg = await bot.send_message(
